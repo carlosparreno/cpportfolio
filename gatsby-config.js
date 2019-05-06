6c09d8e02718b22df59e55654641741dfdbf2054
Carlos Parreño <carlosparreno@Carloss-MacBook-Pro-2.local>
2019-05-06 17:26:51 +0100

    :heavy_plus_sign: add flow

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,13 +4,14 @@ module.exports = {
     description: 'This is the project for my porftolio',
   },
   plugins: [
-    `gatsby-transformer-remark`,
+    'gatsby-transformer-remark',
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: `pages`,
+        name: 'pages',
         path: `${__dirname}/src/pages`,
       },
     },
+    'gatsby-plugin-flow',
   ],
 };
