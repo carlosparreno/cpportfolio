404f79fe6d88f76cae32337830f0a21e5223f895
Carlos Parreño <carlos.parreno@hotmail.es>
2019-05-15 22:39:57 +0100

    :art: Change theme colors

--- colors.js
+++ colors.js
@@ -2,11 +2,11 @@ module.exports = {
   background: '#FFFFFF',
   backgroundDark: '#f0e6f6',
 
-  primary: '#7c37ad',
-  primaryLight: '#ae66df',
-  primaryDark: '#4b007d',
+  primary: '#062F4F',
+  primaryLight: '#0d65aa',
+  primaryDark: '#021320',
 
-  secondary: '#ff4081',
-  secondaryLight: '#ff79b0',
-  secondaryDark: '#c60055',
+  secondary: '#B82601',
+  secondaryLight: '#fe3b0a',
+  secondaryDark: '#6a1601',
 };
