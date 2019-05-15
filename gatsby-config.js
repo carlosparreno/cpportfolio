22589f4754dcf0a4ce8514a93a60e1ae6682ffc5
Carlos Parreño <carlosparreno@Carloss-MacBook-Pro-2.local>
2019-05-15 07:28:28 +0100

    :hammer: Refactor content

--- gatsby-config.js
+++ gatsby-config.js
@@ -32,7 +32,7 @@ module.exports = {
       resolve: 'gatsby-source-filesystem',
       options: {
         name: 'content',
-        path: `${__dirname}/src/pages/content`,
+        path: `${__dirname}/src/content`,
       },
     },
     'gatsby-plugin-flow',
