5e69d9e6810cae6ac78d776b584032adb3809287
Carlos Parreño <carlos.parreno@hotmail.es>
2019-05-16 21:57:03 +0100

    :hammer: Refactor Projects section

---
+++ src/components/ProjectCard.js
@@ -0,0 +1,142 @@
+// @flow
+import React from 'react';
+import { Image, Text, Flex, Box } from 'rebass';
+import styled from 'styled-components';
+import { Card } from './Card';
+import SocialLink from './SocialLink';
+import ImageSubtitle from './ImageSubtitle';
+import Hide from './Hide';
+
+const CARD_HEIGHT = '200px';
+
+const MEDIA_QUERY_SMALL = '@media (max-width: 400px)';
+
+const Title = styled(Text)`
+  font-size: 14px;
+  font-weight: 600;
+  text-transform: uppercase;
+  display: table;
+  border-bottom: ${props => props.theme.colors.primary} 5px solid;
+`;
+
+const TextContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 10px;
+  width: 100%;
+  width: calc(100% - ${CARD_HEIGHT});
+
+  ${MEDIA_QUERY_SMALL} {
+    width: calc(100% - (${CARD_HEIGHT} / 2));
+  }
+`;
+
+const ImageContainer = styled.div`
+  margin: auto;
+  width: ${CARD_HEIGHT};
+
+  ${MEDIA_QUERY_SMALL} {
+    width: calc(${CARD_HEIGHT} / 2);
+  }
+`;
+
+const ProjectImage = styled(Image)`
+  width: ${CARD_HEIGHT};
+  height: ${CARD_HEIGHT};
+  padding: 40px;
+  margin-top: 0px;
+
+  ${MEDIA_QUERY_SMALL} {
+    height: calc(${CARD_HEIGHT} / 2);
+    width: calc(${CARD_HEIGHT} / 2);
+    margin-top: calc(${CARD_HEIGHT} / 4);
+    padding: 10px;
+  }
+`;
+
+const ProjectTag = styled.div`
+  position: relative;
+  height: ${CARD_HEIGHT};
+  top: calc(
+    -${CARD_HEIGHT} - 3.5px
+  ); /*don't know why I have to add 3.5px here ... */
+
+  ${MEDIA_QUERY_SMALL} {
+    top: calc(-${CARD_HEIGHT} - 3.5px + (${CARD_HEIGHT} / 4));
+  }
+`;
+
+type PropTypes = {
+  name: string,
+  description: string,
+  projectUrl: string,
+  repositoryUrl: string,
+  type: string,
+  publishedDate: string,
+  logo: { title: string, src: string },
+};
+
+const ProjectCard = ({
+  name,
+  description,
+  projectUrl,
+  repositoryUrl,
+  type,
+  publishedDate,
+  logo,
+}: PropTypes) => (
+  <Card p={0}>
+    <Flex style={{ height: CARD_HEIGHT }}>
+      <TextContainer>
+        <span>
+          <Title my={2} pb={1}>
+            {name}
+          </Title>
+        </span>
+        <Text width={[1]} style={{ overflow: 'auto' }}>
+          {description}
+        </Text>
+      </TextContainer>
+
+      <ImageContainer>
+        <ProjectImage src={logo.src} alt={logo.title} />
+        <ProjectTag>
+          <Flex
+            style={{
+              float: 'right',
+            }}
+          >
+            <Box mx={1} fontSize={5}>
+              <SocialLink
+                name="Check repository"
+                fontAwesomeIcon="github"
+                url={repositoryUrl}
+              />
+            </Box>
+            <Box mx={1} fontSize={5}>
+              <SocialLink
+                name="See project"
+                fontAwesomeIcon="globe"
+                url={projectUrl}
+              />
+            </Box>
+          </Flex>
+          <ImageSubtitle
+            bg="primaryLight"
+            color="white"
+            y="bottom"
+            x="right"
+            round
+          >
+            {type}
+          </ImageSubtitle>
+          <Hide query={MEDIA_QUERY_SMALL}>
+            <ImageSubtitle bg="backgroundDark">{publishedDate}</ImageSubtitle>
+          </Hide>
+        </ProjectTag>
+      </ImageContainer>
+    </Flex>
+  </Card>
+);
+
+export default ProjectCard;
